798055b48d025602676510532cd72d8b21b7d3e0
Nammyoung <hongnammyoung@gmail.com>
2019-07-06 18:36:58 +0200

    first add. title

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,8 +49,8 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Nam\'s blog',
+  description: '도시의 시공간을 연구합니다',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
