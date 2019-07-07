990ce5f250c23afc05b6fe452c8ea7876b5ca1e5
Nammyoung <hongnammyoung@gmail.com>
2019-07-07 23:47:09 +0200

    change sns url and the name of the page

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,14 +49,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Nam\'s blog',
+  description: '도시의 시공간을 연구합니다.',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://nammyounghong.github.it',
+  facebook: 'https://www.facebook.com/nammyounghong',
+  twitter: 'https://twitter.com/dangmu7722',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
